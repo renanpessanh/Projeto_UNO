e16d6b652a00635cfb6377b57be071fe7be77d30
Renan Pessanha <142458180+renanpessanh@users.noreply.github.com>
2024-01-22 12:18:42 -0300

    Primeiro commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,44 @@
+import styles from "./header.module.css";
+import logo from "../../assets/img/Uno-Logo-menu.png";
+
+const Header = () => {
+  return (
+    <header className={styles.Header}>
+      <div class={styles.header_content}>
+        <div class={styles.header_logo}>
+          <a href="#">
+            <img src={logo} alt="Logo" />
+          </a>
+        </div>
+        <div class={styles.header_nav}>
+          <nav>
+            <ul class={styles.ul_menu}>
+              <li>
+                <a href="#">Início</a>
+              </li>
+              <li>
+                <a href="#">Onde encontrar</a>
+              </li>
+              <li>
+                <a href="#">Novos produtos</a>
+              </li>
+              <li>
+                <a href="#">Créditos</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class={styles.header_button}>
+          <div class={styles.entrar_btn}>
+            <a href="#">Entrar</a>
+          </div>
+          <div class={styles.registro_btn}>
+            <a href="#">Cadastre-se</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
