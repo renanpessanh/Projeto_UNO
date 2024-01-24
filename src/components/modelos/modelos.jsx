24caac527708bbd14ddc7bde5536262c935e1044
Renan Pessanha <142458180+renanpessanh@users.noreply.github.com>
2024-01-24 11:40:39 -0300

    Terminei a section modelos
    Fiz a section mas tem alguns bug, como a sobra de espaço do lado do azul;

--- src/components/modelos/modelos.jsx
+++ src/components/modelos/modelos.jsx
@@ -13,30 +13,85 @@ const Modelos = () => {
           <div class={styles.modelos_content}>
             <div class={styles.modelos_text}>
               <h2 class={styles.modelos_title}>Nossos novos modelos</h2>
-              <a href="#">
-                <p class={styles.modelos_more}>
-                  Ver todos
+              <div class={styles.modelos_more}>
+                <a class={styles.modelos_p} href="#">
+                  <p>Ver todos </p>
                   <arrow>
                     <img src={arrow} alt="seta" />
                   </arrow>
-                </p>
-              </a>
+                </a>
+              </div>
             </div>
           </div>
         </div>
         <div class={styles.modelos_button}>
           <div class={styles.modelos_img}>
             <a href="#">
-              <img src={Uno_minimalista} alt="Uno Minimalista" />
+              <div class={styles.uno_min}>
+                <div class={styles.uno_min_img}>
+                  <img src={Uno_minimalista} alt="Uno Minimalista" />
+                </div>
+
+                <div class={styles.uno_min_text}>
+                  <p>UNO® minimalista</p>
+                  <p2>R$ 80</p2>
+                </div>
+
+                <div class={styles.uno_min_text2}>
+                  <p3>Acaba em 01.34.45</p3>
+                </div>
+              </div>
             </a>
+
             <a href="#">
-              <img src={Uno_flip} alt="Uno Flip" />
+              <div class={styles.uno_roxo}>
+                <div class={styles.uno_roxo_img}>
+                  <img src={Uno_flip} alt="Uno Flip" />
+                </div>
+
+                <div class={styles.uno_roxo_text}>
+                  <p>UNO® minimalista</p>
+                  <p2>R$ 80</p2>
+                </div>
+
+                <div class={styles.uno_roxo_text2}>
+                  <p3>Acaba em 01.34.45</p3>
+                </div>
+              </div>
             </a>
+
             <a href="#">
-              <img src={Uno_default} alt="Uno Default" />
+              <div class={styles.uno_red}>
+                <div class={styles.uno_red_img}>
+                  <img src={Uno_default} alt="Uno Default" />
+                </div>
+
+                <div class={styles.uno_red_text}>
+                  <p>UNO® minimalista</p>
+                  <p2>R$ 80</p2>
+                </div>
+
+                <div class={styles.uno_red_text2}>
+                  <p3>Acaba em 01.34.45</p3>
+                </div>
+              </div>
             </a>
+
             <a href="#">
-              <img src={Uno_dos} alt="Uno Dos" />
+              <div class={styles.uno_azul}>
+                <div class={styles.uno_azul_img}>
+                  <img src={Uno_dos} alt="Uno Dos" />
+                </div>
+
+                <div class={styles.uno_azul_text}>
+                  <p>UNO® minimalista</p>
+                  <p2>R$ 80</p2>
+                </div>
+
+                <div class={styles.uno_azul_text2}>
+                  <p3>Acaba em 01.34.45</p3>
+                </div>
+              </div>
             </a>
           </div>
         </div>
